74e49f4ac27bdd11c9d43f8410279d4d85a67b78
Polly <973367375@qq.com>
2018-04-30 14:00:15 +0800

    u

---
+++ thzupdate/index.js
@@ -0,0 +1,11 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const fs = require('fs')
+
+request('http://thz.la/forum-220-2.html',function(err, res, body) {
+    if (err) {
+        console.log(err)
+    } else {
+        
+    }
+})
