a5c6ffe5d3f1302bec49c175b499977041cc1085
Polly <973367375@qq.com>
2018-05-02 18:03:32 +0800

    u

--- thzupdate/draft.js
+++ thzupdate/draft.js
@@ -1,6 +1,9 @@
 const request = require('request')
+const cheerio = require('cheerio')
 const fs = require('fs')
 
-request('http://taohuabt.cc/', (err, res, body) => {
+request('http://taohuabt.cc/thread-1814403-1-2.html', (err, res, body) => {
     fs.writeFileSync('index.html', body)
 })
+// request('http://pic.thzpic.com/forum/201805/01/190705i4kdndmkdgb4x8mb.jpg')
+// .pipe(fs.createWriteStream('test.jpg'))
--- thzupdate/index.js
+++ thzupdate/index.js
@@ -26,6 +26,7 @@ function fetch(uri) {
 function update() {
     return new Promise(async (resolve, reject) => {
         let start = 1
+        let update = true
         const tmpData = {}
         while (start < 100) {
             console.log(start)
@@ -35,13 +36,24 @@ function update() {
                 continue
             }
             const $ = cheerio.load(html)
-            $('a.s.xst').each((_, el) => {
+            const $new = $('.new')
+
+            $new.find('a.s.xst').each((_, el) => {
+                const date = $new.eq(_).next().find('span').eq(1).attr('title')
+                const gap = (Date.now() - new Date(date.replace(/\-/g, '/'))) / (24 * 60 * 60 * 1000)
+                if (Math.floor(gap) > 1) {
+                    update = false
+                    return
+                }
                 const $el = $(el)
                 const key = $el.attr('href')
                 if (key.indexOf('thread-213795-1-1') === -1) {
                     tmpData[key] = base64.encode(utf8.encode($el.text()))
                 }
             })
+            if (!update) {
+                break;
+            }
             start++
             await sleep(2000)
         }
@@ -57,9 +69,11 @@ function fetchAndGetId(uri) {
         } else {
             const $ = cheerio.load(html)
             const $a = $('.attnm>a')
+            const cover = $('.zoom').eq(0).attr('file')
             resolve({
                 id: $a.attr('href').split(',')[1],
-                name: $a.text()
+                name: $a.text(),
+                cover,
             })
         }
     })
@@ -67,7 +81,7 @@ function fetchAndGetId(uri) {
 }
 
 // update().then(data => {
-//     jsonfile.writeFileSync('./data/feed.json', data, {flag: 'a'})
+//     jsonfile.writeFileSync('./data/update.json', data, {flag: 'a'})
 // })
 // request('http://taohuabt.cc/thread-977162-1-1.html', (err, res, body) => {
 //     if (err) {
@@ -80,15 +94,27 @@ function fetchAndGetId(uri) {
 function decode(str) {
     return utf8.decode(base64.decode(str))
 }
-fetchAndGetId(`${baseUri}thread-977162-1-1.html`).then(res => {
-    if (!res) {
-        console.log('something wrong')
-        return false
+const latestData = jsonfile.readFileSync('./data/update.json')
+// Object.keys(latestData).forEach(key => {
+//     console.log(latestData[key])
+// })
+
+async function getTorrent(data) {
+    const keys = Object.keys(data)
+    for (let i = 0; i < keys.length; i++) {
+        const uri = `${baseUri}${keys[i]}`
+        const response = await fetchAndGetId(uri)
+        try {
+            request(`${baseUri}forum.php?mod=attachment&aid=${response.id}`)
+                .pipe(fs.createWriteStream(`./data/resource/${response.name}`))
+            request(response.cover).pipe(fs.createWriteStream(`./data/resource/${response.name}.jpg`))
+        } catch (err) {
+            console.log(err)
+        }
+
+        await sleep(12000)
+
     }
-    try {
-        request(`${baseUri}forum.php?mod=attachment&aid=${res.id}`)
-            .pipe(fs.createWriteStream(`./data/${res.name}`))
-    } catch (err) {
-        console.log(err)
-    }
-})
+}
+
+getTorrent(latestData)
